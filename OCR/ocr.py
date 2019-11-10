68687fa1c33cca00575e3ee38ae699589cedcb2d
Sourav Sharan <souravthesharan@gmail.com>
2019-11-10 07:01:15 +0530

    ocr func

--- OCR/ocr.py
+++ OCR/ocr.py
@@ -1,16 +1,19 @@
 import requests
+import json
 
-url = "https://taggun.p.rapidapi.com/api/receipt/v1/verbose/url"
+def ocr(imageUrl):
+    url = "https://taggun.p.rapidapi.com/api/receipt/v1/verbose/url"
+    #imageUrl = "https://courses.cs.vt.edu/csonline/AI/Lessons/VisualProcessing/OCRscans_files/robertson.jpg"
+    payload = "{\"url\":\"" + imageUrl+ "\",\"headers\":{},\"refresh\":false,\"incognito\":false,\"ipAddress\":\"32.4.2.223\"}"
+    headers = {
+        'x-rapidapi-host': "taggun.p.rapidapi.com",
+        'x-rapidapi-key': "b7f27d3d50mshd9bf5f0c086a683p136f85jsnc22917a3a34a",
+        'content-type': "application/json",
+        'accept': "application/json"
+        }
 
-payload = "{\"url\":\"https://courses.cs.vt.edu/csonline/AI/Lessons/VisualProcessing/OCRscans_files/robertson.jpg\",\"headers\":{},\"refresh\":false,\"incognito\":false,\"ipAddress\":\"32.4.2.223\"}"
-headers = {
-    'x-rapidapi-host': "taggun.p.rapidapi.com",
-    'x-rapidapi-key': "b7f27d3d50mshd9bf5f0c086a683p136f85jsnc22917a3a34a",
-    'content-type': "application/json",
-    'accept': "application/json"
-    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    output = json.loads(response.text)
 
-response = requests.request("POST", url, data=payload, headers=headers)
-output = json.loads(response.text)
+    print('The text in front of you is :    ' + output['text']['text'])
 
-print('The text in front of you is :    ' + output['text']['text'])
