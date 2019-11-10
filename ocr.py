8998a846a7af2f7a03ddb6c22b248cd362d96521
Varun Ghatrazu <varunghatrazu@gmail.com>
2019-11-10 06:23:24 +0530

    Add files via upload

---
+++ ocr.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://taggun.p.rapidapi.com/api/receipt/v1/verbose/url"
+
+payload = "{\"url\":\"https://courses.cs.vt.edu/csonline/AI/Lessons/VisualProcessing/OCRscans_files/robertson.jpg\",\"headers\":{},\"refresh\":false,\"incognito\":false,\"ipAddress\":\"32.4.2.223\"}"
+headers = {
+    'x-rapidapi-host': "taggun.p.rapidapi.com",
+    'x-rapidapi-key': "b7f27d3d50mshd9bf5f0c086a683p136f85jsnc22917a3a34a",
+    'content-type': "application/json",
+    'accept': "application/json"
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers)
+output = json.loads(response.text)
+
+print('The text in front of you is :    ' + output['text']['text'])
